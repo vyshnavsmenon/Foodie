6f2d6190265396499b40c5c521f79ea58e00ac16
Vyshnav S Menon <vyshnavsmenon62@gmail.com>
2025-01-12 12:00:11 +0530

    ...

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:foodie/models/main.dart';
+import 'package:foodie/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
